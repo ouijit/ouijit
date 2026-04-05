b011378da4a48d174d51d824e228802e02fbfb62
pbjer <prenticebjerk@gmail.com>
2026-04-04 18:11:40 -0700

    Fix launch-editor/guess not found in packaged app
    CJS require('launch-editor/guess') was left as a runtime call by Vite,
    failing in the packaged .app where node_modules doesn't exist. Convert
    to a static import so Vite bundles it.

--- src/editorLauncher.ts
+++ src/editorLauncher.ts
@@ -10,8 +10,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { execFileSync, spawn } from 'node:child_process';
 import launchEditor from 'launch-editor';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const guessEditor = require('launch-editor/guess') as (specifiedEditor?: string) => string[];
+import guessEditor from 'launch-editor/guess';
 import { getHook } from './db';
 import log from './log';
 
